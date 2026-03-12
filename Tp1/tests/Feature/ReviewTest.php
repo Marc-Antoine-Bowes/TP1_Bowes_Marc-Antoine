75ec1e91f8908abe3ac3afcfe5ba0590820e82eb
Padawanb <padawanb@hotmail.com>
2026-03-12 15:06:02 -0400

    Test fin

--- Tp1/tests/Feature/ReviewTest.php
+++ Tp1/tests/Feature/ReviewTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use App\Models\Review;
 use Tests\TestCase;
 
 class ReviewTest extends TestCase
@@ -11,10 +12,24 @@ class ReviewTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_example(): void
+    /*public function test_example(): void
     {
         $response = $this->get('/');
 
         $response->assertStatus(200);
-    }
+    }*/
+
+public function test_destroy_deletes_review()
+{
+    //Avec l'aide de l'ia  prompt : comment faire un test sur un delete en laravel
+    $review = Review::factory()->create();
+
+    $response = $this->deleteJson("/api/reviews/{$review->id}");
+
+    $response->assertStatus(204);
+
+    $this->assertDatabaseMissing('reviews', [
+        'id' => $review->id
+    ]);
+}
 }
